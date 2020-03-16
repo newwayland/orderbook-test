778c80cde29ce275082c2d396ffd77f88a74bd58
Neil Wilson <neil@aldur.co.uk>
2020-03-16 08:21:52 +0000

    Inital Commit

---
+++ src/Main.elm
@@ -0,0 +1,96 @@
+module Main exposing (..)
+-- Show the current time in your time zone.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/effects/time.html
+--
+-- For an analog clock, check out this SVG example:
+--   https://elm-lang.org/examples/clock
+--
+
+import Browser
+import Html exposing (..)
+import Task
+import Time
+
+
+
+-- MAIN
+
+
+main =
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { zone : Time.Zone
+  , time : Time.Posix
+  }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Model Time.utc (Time.millisToPosix 0)
+  , Task.perform AdjustTimeZone Time.here
+  )
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Tick Time.Posix
+  | AdjustTimeZone Time.Zone
+
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Tick newTime ->
+      ( { model | time = newTime }
+      , Cmd.none
+      )
+
+    AdjustTimeZone newZone ->
+      ( { model | zone = newZone }
+      , Cmd.none
+      )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every 1000 Tick
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  let
+    hour   = Time.toHour model.zone model.time
+            |> String.fromInt 
+    minute = Time.toMinute model.zone model.time
+            |> String.fromInt
+            |> String.padLeft 2 '0'
+    second = Time.toSecond model.zone model.time
+            |> String.fromInt
+            |> String.padLeft 2 '0'
+  in
+  h1 [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ]
