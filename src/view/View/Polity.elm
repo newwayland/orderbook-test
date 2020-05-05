0de953e9fa007cfcd8c02717e2eca0bf6b9a0ffd
Neil Wilson <neil@aldur.co.uk>
2020-05-05 19:16:37 +0100

    Switch age categories to select list

--- src/view/View/Polity.elm
+++ src/view/View/Polity.elm
@@ -3,7 +3,9 @@ module View.Polity exposing (Polity, viewAgeCategories)
 import Bootstrap.Card.Block as Block
 import Bootstrap.Form as Form
 import Bootstrap.Form.Input as Input
-import Html.Attributes exposing (placeholder, value)
+import Bootstrap.Form.Select as Select
+import Html exposing (text)
+import Html.Attributes exposing (placeholder, selected, value)
 import Html.Events exposing (onInput)
 import Model.Polity
 import Update exposing (Msg(..))
@@ -21,38 +23,46 @@ viewAgeCategories : Polity -> Block.Item Msg
 viewAgeCategories settings =
     Block.custom <|
         Form.formInline []
-            [ Input.number
-                [ Input.id "schoolAge"
-                , Model.Polity.schoolAge settings |> String.fromInt |> Input.value
-                , Input.placeholder "School Age"
-                , Input.small
-                , Input.onInput ChangeSchoolAge
-                , Input.attrs
-                    [ Html.Attributes.min "0"
-                    , Model.Polity.majority settings - 1 |> String.fromInt |> Html.Attributes.max
-                    , Html.Attributes.pattern "[0-9]*"
+            [ generateRange
+                0
+                (Model.Polity.majority settings - 1)
+                (Model.Polity.schoolAge settings)
+                |> Select.select
+                    [ Select.id "schoolAge"
+                    , Select.small
+                    , Select.onChange ChangeSchoolAge
                     ]
-                ]
-            , Input.number
-                [ Input.id "workingAge"
-                , Model.Polity.majority settings |> String.fromInt |> Input.value
-                , Input.placeholder "Working Age"
-                , Input.small
-                , Input.onInput ChangeWorkingAge
-                , Input.attrs
-                    [ Model.Polity.schoolAge settings + 1 |> String.fromInt |> Html.Attributes.min
-                    , Model.Polity.retirementAge settings - 1 |> String.fromInt |> Html.Attributes.max
+            , generateRange
+                (Model.Polity.schoolAge settings + 1)
+                (Model.Polity.retirementAge settings - 1)
+                (Model.Polity.majority settings)
+                |> Select.select
+                    [ Select.id "workingAge"
+                    , Select.small
+                    , Select.onChange ChangeWorkingAge
                     ]
-                ]
-            , Input.number
-                [ Input.id "retirementAge"
-                , Model.Polity.retirementAge settings |> String.fromInt |> Input.value
-                , Input.placeholder "RetirementAge"
-                , Input.small
-                , Input.onInput ChangeRetirementAge
-                , Input.attrs
-                    [ Model.Polity.majority settings + 1 |> String.fromInt |> Html.Attributes.min
-                    , Html.Attributes.max "120"
+            , generateRange
+                (Model.Polity.majority settings + 1)
+                120
+                (Model.Polity.retirementAge settings)
+                |> Select.select
+                    [ Select.id "workingAge"
+                    , Select.small
+                    , Select.onChange ChangeRetirementAge
                     ]
-                ]
             ]
+
+
+generateRange : Int -> Int -> Int -> List (Select.Item msg)
+generateRange min max current =
+    List.range min max
+        |> List.map (\x -> generateItem x (x == current))
+
+
+generateItem : Int -> Bool -> Select.Item msg
+generateItem element isSelected =
+    let
+        elementStr =
+            String.fromInt element
+    in
+    Select.item [ value elementStr, selected isSelected ] [ text elementStr ]
